f22d1d9fa940d4f6addddeb1020728e3948a975d
felix <felix@xing.com>
2026-04-16 18:04:35 +0800

    增加了开始和结束按钮

--- src/self_button.rs
+++ src/self_button.rs
@@ -2,12 +2,32 @@
 use bevy::color::palettes::basic::*;
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct EnumUi;
+
+#[derive(Component)]
+struct StartGameUi;
+
+#[derive(Component)]
+struct StopGameUi;
+
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, States)]
+enum Appstatus {
+    #[default] // 标记默认状态是 Setup
+    Menu,
+    Image,
+}
+
 
 pub fn self_run() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .init_state::<Appstatus>()
         .add_systems(Startup, setup)
-        .add_systems(Update, button_systems)
+        .add_systems(Update, start_button_systems.run_if(in_state(Appstatus::Menu)))
+        .add_systems(Update, stop_button_systems.run_if(in_state(Appstatus::Menu)))
+        .add_systems(OnEnter(Appstatus::Image), show_image)
         .run();
 }
 
@@ -26,8 +46,11 @@ fn self_button(asset: &AssetServer) -> impl Bundle {
             height: percent(100),
             align_items: AlignItems::Center, // 子节点垂直居中 
             justify_content: JustifyContent::Center, // 子节点水平居中 
+            flex_direction: FlexDirection::Column, // 子元素垂直排列
+            row_gap: px(20.0), // 垂直间隔
             ..default()
         },
+        EnumUi,
         children![ //宏，自动创建子节点
             (
                 Button,
@@ -40,13 +63,45 @@ fn self_button(asset: &AssetServer) -> impl Bundle {
                     align_items: AlignItems::Center,
                     ..default()
                 },
+                StartGameUi,
                 BorderColor::all(Color::WHITE), //边框颜色,白色
                 //BorderRadius::MAX, // 最大圆角，形成一个圆角矩形，最大圆角是胶囊圆角
                 BorderRadius::all(px(8.0)), // 8像素的圆角半径，更现代
                 BackgroundColor(Color::BLACK),
                 children![
                     (
-                        Text::new("开始"),
+                        Text::new("开始游戏"),
+                        TextFont {
+                            font: asset.load("fonts/STKAITI.TTF"),
+                            font_size: 33.0,
+                            ..default()
+                        },
+                        TextColor(Color::srgb(0.9, 0.9, 0.9)), //文本颜色
+                        //TextShadow::default()  //字体阴影
+                        
+                    )
+                ]
+
+            ),
+            (
+                Button,
+                Node {
+                    width: px(150), // 宽 150像素
+                    height: px(65), // 高65像素
+                    border: UiRect::all(px(5)),  // UiRect 是四个边的矩形，all 是这是四周宽度都是5个像素；
+                                                 // border 定义边框宽度
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                StopGameUi,
+                BorderColor::all(Color::WHITE), //边框颜色,白色
+                //BorderRadius::MAX, // 最大圆角，形成一个圆角矩形，最大圆角是胶囊圆角
+                BorderRadius::all(px(8.0)), // 8像素的圆角半径，更现代
+                BackgroundColor(Color::BLACK),
+                children![
+                    (
+                        Text::new("结束游戏"),
                         TextFont {
                             font: asset.load("fonts/STKAITI.TTF"),
                             font_size: 33.0,
@@ -66,7 +121,61 @@ fn self_button(asset: &AssetServer) -> impl Bundle {
 const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
 const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
-fn button_systems(
+
+
+fn start_button_systems(
+    // Interaction 是记录UI实体与鼠标或触摸之间的交互状态；
+    // 他有三个状态，无交互，悬停和按下
+    mut commands: Commands,
+    mut interaction_query: Query<( //查找同时包含这么多组件的实体，
+            &Interaction,  //Interaction会自动添加到button组件的实体上
+            &mut BackgroundColor, 
+            &mut BorderColor,
+            &mut Button,
+            &Children,
+        ), (Changed<Interaction>, With<StartGameUi>) >, // 添加限定条件，Interaction变化时
+    mut text_query: Query<&mut Text>,
+    menu_entity: Query<Entity, With<EnumUi>>,
+    mut next_status: ResMut<NextState<Appstatus>>
+) {
+    for ( interaction, mut background_color, mut border_color, mut button, children ) in interaction_query.iter_mut() {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        // 这里 text_query.get_mut(entity) 是用来获取实体中的Text组件；
+        // 正常情况下 text_query是获取全局的Text组件
+        match interaction {
+            Interaction::Pressed => {
+                //text.0 = "按下".to_string(); // text是一个Mut<'_,Text>类型，需要解引用才能获得Text
+                background_color.0 = PRESSED_BUTTON.into();
+                border_color.set_all(RED);
+
+
+                //menu_entity
+                if let Ok(menu_en) = menu_entity.single() {
+                    commands.entity(menu_en).despawn_children();
+                    next_status.set(Appstatus::Image);
+                    
+                }
+            }
+            Interaction::Hovered => {
+                //text.0 = "停留".to_string(); // text是一个Mut<'_,Text>类型，需要解引用才能获得Text
+                background_color.0 = HOVERED_BUTTON.into();
+                
+                border_color.set_all(WHITE);
+            }
+            Interaction::None => {
+                //text.0 = "开始".to_string(); // text是一个Mut<'_,Text>类型，需要解引用才能获得Text
+                background_color.0 = NORMAL_BUTTON.into();
+                
+                border_color.set_all(BLACK);
+            }
+        }
+    }
+}
+
+
+
+
+fn stop_button_systems(
     // Interaction 是记录UI实体与鼠标或触摸之间的交互状态；
     // 他有三个状态，无交互，悬停和按下
     mut interaction_query: Query<( //查找同时包含这么多组件的实体，
@@ -75,8 +184,9 @@ fn button_systems(
             &mut BorderColor,
             &mut Button,
             &Children,
-        ), Changed<Interaction>>, // 添加限定条件，Interaction变化时
+        ), (Changed<Interaction>,With<StopGameUi>)>, // 添加限定条件，Interaction变化时
     mut text_query: Query<&mut Text>,
+    mut app_exit_events: MessageWriter<AppExit>,
 ) {
     for ( interaction, mut background_color, mut border_color, mut button, children ) in interaction_query.iter_mut() {
         let mut text = text_query.get_mut(children[0]).unwrap();
@@ -84,23 +194,38 @@ fn button_systems(
         // 正常情况下 text_query是获取全局的Text组件
         match interaction {
             Interaction::Pressed => {
-                text.0 = "按下".to_string(); // text是一个Mut<'_,Text>类型，需要解引用才能获得Text
+                //text.0 = "按下".to_string(); // text是一个Mut<'_,Text>类型，需要解引用才能获得Text
                 background_color.0 = PRESSED_BUTTON.into();
                 
                 border_color.set_all(RED);
+
+                app_exit_events.write(AppExit::Success);
+
+
             }
             Interaction::Hovered => {
-                text.0 = "停留".to_string(); // text是一个Mut<'_,Text>类型，需要解引用才能获得Text
+                //text.0 = "停留".to_string(); // text是一个Mut<'_,Text>类型，需要解引用才能获得Text
                 background_color.0 = HOVERED_BUTTON.into();
                 
                 border_color.set_all(WHITE);
             }
             Interaction::None => {
-                text.0 = "开始".to_string(); // text是一个Mut<'_,Text>类型，需要解引用才能获得Text
+                //text.0 = "开始".to_string(); // text是一个Mut<'_,Text>类型，需要解引用才能获得Text
                 background_color.0 = NORMAL_BUTTON.into();
                 
                 border_color.set_all(BLACK);
             }
         }
     }
+}
+
+
+fn show_image( mut commands : Commands, assert_server: Res<AssetServer>) {
+    commands.spawn(
+      Sprite::from_image(
+        assert_server.load("my_photo.jpeg") 
+      ) 
+    );
+
+
 }
